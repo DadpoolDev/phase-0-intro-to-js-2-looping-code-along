73daf6afd2899a1e3753f500356e819ac3d050a4
Phil <p.mcdonald.jr@gmail.com>
2021-03-20 22:55:34 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(names, event){
+    const messages = []
+
+    //loop for names and adding custom thank you not to array
+
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+    }
+    return messages
+}
+
+function countDown(number){
+    for (let i = number; i > 0; i--) {
+        console.log(i);  
+    }
+    console.log(0)
+
+}
+
